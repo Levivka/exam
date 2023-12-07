82c7fae641734bba996b2dc70c0802ccf7af18af
egor.levitskiy <egor.levitskiy@LT7>
2023-12-07 03:24:31 +0300

    Добавьте файлы проекта.

---
+++ exam2try/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace exam2try
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mainPanel = new Panel();
+            currencyDG = new DataGridView();
+            name = new DataGridViewTextBoxColumn();
+            reduction = new DataGridViewTextBoxColumn();
+            convert = new DataGridViewTextBoxColumn();
+            convertBT = new Button();
+            currencyResultCB = new ComboBox();
+            currencyResultLB = new Label();
+            currencyCB = new ComboBox();
+            currencyTB = new TextBox();
+            mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)currencyDG).BeginInit();
+            SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            mainPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            mainPanel.AutoScroll = true;
+            mainPanel.Controls.Add(currencyDG);
+            mainPanel.Controls.Add(convertBT);
+            mainPanel.Controls.Add(currencyResultCB);
+            mainPanel.Controls.Add(currencyResultLB);
+            mainPanel.Controls.Add(currencyCB);
+            mainPanel.Controls.Add(currencyTB);
+            mainPanel.Location = new Point(0, 0);
+            mainPanel.Name = "mainPanel";
+            mainPanel.Size = new Size(800, 450);
+            mainPanel.TabIndex = 5;
+            // 
+            // currencyDG
+            // 
+            currencyDG.AllowUserToAddRows = false;
+            currencyDG.AllowUserToDeleteRows = false;
+            currencyDG.AllowUserToResizeColumns = false;
+            currencyDG.AllowUserToResizeRows = false;
+            currencyDG.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            currencyDG.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            currencyDG.Columns.AddRange(new DataGridViewColumn[] { name, reduction, convert });
+            currencyDG.Location = new Point(0, 262);
+            currencyDG.Name = "currencyDG";
+            currencyDG.ReadOnly = true;
+            currencyDG.RowHeadersWidth = 51;
+            currencyDG.RowTemplate.Height = 29;
+            currencyDG.Size = new Size(800, 188);
+            currencyDG.TabIndex = 10;
+            // 
+            // name
+            // 
+            name.Frozen = true;
+            name.HeaderText = "Название валюты";
+            name.MinimumWidth = 6;
+            name.Name = "name";
+            name.ReadOnly = true;
+            name.Width = 250;
+            // 
+            // reduction
+            // 
+            reduction.Frozen = true;
+            reduction.HeaderText = "Сокращение названия валюты";
+            reduction.MinimumWidth = 6;
+            reduction.Name = "reduction";
+            reduction.ReadOnly = true;
+            reduction.Width = 250;
+            // 
+            // convert
+            // 
+            convert.Frozen = true;
+            convert.HeaderText = "Курс относительно рубля";
+            convert.MinimumWidth = 6;
+            convert.Name = "convert";
+            convert.ReadOnly = true;
+            convert.Width = 250;
+            // 
+            // convertBT
+            // 
+            convertBT.Location = new Point(253, 163);
+            convertBT.Name = "convertBT";
+            convertBT.Size = new Size(219, 29);
+            convertBT.TabIndex = 9;
+            convertBT.Text = "Конвертировать валюту";
+            convertBT.UseVisualStyleBackColor = true;
+            convertBT.Click += convertBT_Click;
+            // 
+            // currencyResultCB
+            // 
+            currencyResultCB.FormattingEnabled = true;
+            currencyResultCB.Location = new Point(517, 101);
+            currencyResultCB.Name = "currencyResultCB";
+            currencyResultCB.Size = new Size(151, 28);
+            currencyResultCB.TabIndex = 8;
+            // 
+            // currencyResultLB
+            // 
+            currencyResultLB.AutoSize = true;
+            currencyResultLB.Location = new Point(517, 52);
+            currencyResultLB.Name = "currencyResultLB";
+            currencyResultLB.Size = new Size(233, 20);
+            currencyResultLB.TabIndex = 7;
+            currencyResultLB.Text = "Сдесь будет результат перевода";
+            // 
+            // currencyCB
+            // 
+            currencyCB.FormattingEnabled = true;
+            currencyCB.Location = new Point(62, 101);
+            currencyCB.Name = "currencyCB";
+            currencyCB.Size = new Size(151, 28);
+            currencyCB.TabIndex = 6;
+            // 
+            // currencyTB
+            // 
+            currencyTB.Location = new Point(62, 49);
+            currencyTB.Name = "currencyTB";
+            currencyTB.Size = new Size(151, 27);
+            currencyTB.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(mainPanel);
+            Name = "Form1";
+            Text = "Form1";
+            Resize += Form1_Resize;
+            mainPanel.ResumeLayout(false);
+            mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)currencyDG).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel mainPanel;
+        private Button convertBT;
+        private ComboBox currencyResultCB;
+        private Label currencyResultLB;
+        private ComboBox currencyCB;
+        private TextBox currencyTB;
+        private DataGridView currencyDG;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn reduction;
+        private DataGridViewTextBoxColumn convert;
+    }
+}
---
+++ exam2try/Form1.cs
@@ -0,0 +1,95 @@
+using Npgsql;
+using System.Windows.Forms;
+
+namespace exam2try
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlCommand request;
+        string connString = "Host=localhost;Port=4444;Username=admin;Password=admin1;Database=postgres;";
+        List<string[]> data = new List<string[]>();
+        public Form1()
+        {
+            InitializeComponent();
+            currencyDG.Columns[0].Width = this.ClientSize.Width / 3;
+            currencyDG.Columns[1].Width = this.ClientSize.Width / 3;
+            currencyDG.Columns[2].Width = this.ClientSize.Width / 3 - 2;
+            NpgsqlConnection conn = new NpgsqlConnection(connString);
+            conn.Open();
+            request = new NpgsqlCommand("SELECT name FROM currency_data;", conn);
+            using (NpgsqlDataReader reader = request.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+
+                    currencyCB.Items.Add(reader.GetString(0));
+                    currencyResultCB.Items.Add(reader.GetString(0));
+                }
+            }
+            request = new NpgsqlCommand("SELECT name, reduction, convert_value FROM currency_data;", conn);
+            using (NpgsqlDataReader reader = request.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    data.Add(new string[3]);
+
+                    data[data.Count - 1][0] = reader[0].ToString();
+                    data[data.Count - 1][1] = reader[1].ToString();
+                    data[data.Count - 1][2] = reader[2].ToString();
+                }
+
+                conn.Close();
+
+                foreach (string[] s in data)
+                {
+                    currencyDG.Rows.Add(s);
+                }
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            mainPanel.Width = this.ClientSize.Width;
+            convertBT.Width = this.ClientSize.Width / 100 * 30;
+            currencyCB.Width = this.ClientSize.Width / 100 * 30;
+            currencyDG.Width = this.ClientSize.Width;
+            currencyDG.Columns[0].Width = this.ClientSize.Width / 3;
+            currencyDG.Columns[1].Width = this.ClientSize.Width / 3;
+            currencyDG.Columns[2].Width = this.ClientSize.Width / 3;
+            currencyResultCB.Width = this.ClientSize.Width / 100 * 30;
+            currencyResultLB.Width = this.ClientSize.Width / 100 * 30;
+            currencyTB.Width = this.ClientSize.Width / 100 * 30;
+        }
+
+        private void convertBT_Click(object sender, EventArgs e)
+        {
+            double currencyFirstResult = 0, currencySecondResult = 0;
+            if (currencyTB.Text != " " && currencyResultCB != null && currencyCB != null)
+            {
+                NpgsqlConnection conn = new NpgsqlConnection(connString);
+                conn.Open();
+                request = new NpgsqlCommand($"SELECT convert_value FROM currency_data WHERE name = '{currencyCB.Text}';", conn);
+                using (NpgsqlDataReader reader = request.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        currencyFirstResult = reader.GetDouble(0);
+                    }
+                }
+                request = new NpgsqlCommand($"SELECT convert_value FROM currency_data WHERE name = '{currencyResultCB.Text}';", conn);
+                using (NpgsqlDataReader reader = request.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        currencySecondResult = reader.GetDouble(0);
+                    }
+                }
+                double.TryParse(currencyTB.Text, out double currencyAmmount);
+                double currencyFirstRub = currencyFirstResult * currencyAmmount;
+                double finalCurrency = currencyFirstRub / currencySecondResult;
+                currencyResultLB.Text = finalCurrency.ToString();
+            }
+        }
+    }
+}
