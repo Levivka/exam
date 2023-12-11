703ba8b54f76eb61c8028de4166ad23c15d85b55
Egor Levitskiy <egor.levitskiy@net.ik>
2023-12-12 02:20:35 +0300

    Добавлены небольшие фичи

--- exam2try/Form1.cs
+++ exam2try/Form1.cs
@@ -14,55 +14,56 @@ namespace exam2try
         public Form1()
         {
             InitializeComponent();
-            currencyDG.Columns[0].Width = this.ClientSize.Width / 3;
-            currencyDG.Columns[1].Width = this.ClientSize.Width / 3;
-            currencyDG.Columns[2].Width = this.ClientSize.Width / 3 - 2;
-            NpgsqlConnection conn = new NpgsqlConnection(connString);
-            conn.Open();
-            request = new NpgsqlCommand("SELECT name FROM currency_data;", conn);
-            using (NpgsqlDataReader reader = request.ExecuteReader())
+            bool check = true;
+            for (int i = 0; i < currencyDG.RowCount; i++)
             {
-                while (reader.Read())
+                if (Convert.ToDouble(currencyDG[2, i].Value) < 0)
                 {
-
-                    currencyCB.Items.Add(reader.GetString(0));
-                    currencyResultCB.Items.Add(reader.GetString(0));
+                    check = false;
+                    MessageBox.Show($"Ошибка в ячейке {i + 1}");
                 }
             }
-            request = new NpgsqlCommand("SELECT name, reduction, convert_value FROM currency_data;", conn);
-            using (NpgsqlDataReader reader = request.ExecuteReader())
+
+            if (check == true)
             {
-
-                while (reader.Read())
+                NpgsqlConnection conn = new NpgsqlConnection(connString);
+                conn.Open();
+                request = new NpgsqlCommand("SELECT name FROM currency_data;", conn);
+                using (NpgsqlDataReader reader = request.ExecuteReader())
                 {
-                    data.Add(new string[3]);
+                    while (reader.Read())
+                    {
 
-                    data[data.Count - 1][0] = reader[0].ToString();
-                    data[data.Count - 1][1] = reader[1].ToString();
-                    data[data.Count - 1][2] = reader[2].ToString();
+                        currencyCB.Items.Add(reader.GetString(0));
+                        currencyResultCB.Items.Add(reader.GetString(0));
+                    }
                 }
-
-                conn.Close();
-
-                foreach (string[] s in data)
+                request = new NpgsqlCommand("SELECT name, reduction, convert_value FROM currency_data;", conn);
+                using (NpgsqlDataReader reader = request.ExecuteReader())
                 {
-                    currencyDG.Rows.Add(s);
+
+                    while (reader.Read())
+                    {
+                        data.Add(new string[3]);
+
+                        data[data.Count - 1][0] = reader[0].ToString();
+                        data[data.Count - 1][1] = reader[1].ToString();
+                        data[data.Count - 1][2] = reader[2].ToString();
+                    }
+
+                    conn.Close();
+
+                    foreach (string[] s in data)
+                    {
+                        currencyDG.Rows.Add(s);
+                    }
                 }
             }
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            mainPanel.Width = this.ClientSize.Width;
-            convertBT.Width = this.ClientSize.Width / 100 * 30;
-            currencyCB.Width = this.ClientSize.Width / 100 * 30;
-            currencyDG.Width = this.ClientSize.Width;
-            currencyDG.Columns[0].Width = this.ClientSize.Width / 3;
-            currencyDG.Columns[1].Width = this.ClientSize.Width / 3;
-            currencyDG.Columns[2].Width = this.ClientSize.Width / 3;
-            currencyResultCB.Width = this.ClientSize.Width / 100 * 30;
-            currencyResultLB.Width = this.ClientSize.Width / 100 * 30;
-            currencyTB.Width = this.ClientSize.Width / 100 * 30;
+
         }
 
         private void convertBT_Click(object sender, EventArgs e)
@@ -90,7 +91,8 @@ namespace exam2try
                 }
                 double.TryParse(currencyTB.Text, out double currencyAmmount);
                 double finalCurrency = (currencyFirstResult * currencyAmmount) / currencySecondResult;
-                currencyResultLB.Text = finalCurrency.ToString();
+                double finalCurrencyRounded = Math.Round(finalCurrency, 4);
+                currencyResultLB.Text = finalCurrencyRounded.ToString();
             }
         }
 
@@ -98,42 +100,56 @@ namespace exam2try
         {
             List<Currency> currencyList = new List<Currency>();
             DateTime today = DateTime.Today;
-
-            foreach (DataGridViewRow row in currencyDG.Rows)
+            bool check = true;
+            for (int i = 0; i < currencyDG.RowCount; i++)
             {
-                Currency currency = new Currency
+                if (Convert.ToDouble(currencyDG[2, i].Value) < 0)
                 {
-                    CurrencyName = row.Cells["name"].Value.ToString(),
-                    CurrencyReduction = row.Cells["reduction"].Value.ToString(),
-                    СurrencyVal = Convert.ToDouble(row.Cells["convert"].Value)
-                };
-                currencyList.Add(currency);
-            }
-
-            var datasave = new Save()
-            {
-                Date = today,
-                Data = currencyList.ToArray()
-            };
-
-            var json = JsonConvert.SerializeObject(datasave, Newtonsoft.Json.Formatting.Indented);
-            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "exam");
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-            try
-            {
-                using (StreamWriter sr = new StreamWriter(Path.Combine(path, "save.json")))
-                {
-                    sr.WriteLine(json);
-                    MessageBox.Show($"Запись в файл по пути {path} прошла успешна");
+                    check = false;
+                    MessageBox.Show($"Ошибка в ячейке {i + 1}");
                 }
             }
-            catch (Exception ex)
+
+            if (check == true)
             {
-                MessageBox.Show($"Не получилось записать данные в файл код ошибки {ex}");
+                foreach (DataGridViewRow row in currencyDG.Rows)
+                {
+                    Currency currency = new Currency
+                    {
+
+                        CurrencyName = row.Cells["name"].Value.ToString(),
+                        CurrencyReduction = row.Cells["reduction"].Value.ToString(),
+                        СurrencyVal = Convert.ToDouble(row.Cells["convert"].Value)
+                    };
+                    currencyList.Add(currency);
+                }
+
+                    var datasave = new Save()
+                    {
+                        Date = today,
+                        Data = currencyList.ToArray()
+                    };
+
+                    var json = JsonConvert.SerializeObject(datasave, Newtonsoft.Json.Formatting.Indented);
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "exam");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    try
+                    {
+                        using (StreamWriter sr = new StreamWriter(Path.Combine(path, "save.json")))
+                        {
+                            sr.WriteLine(json);
+                            MessageBox.Show($"Запись в файл по пути {path} прошла успешна");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Не получилось записать данные в файл код ошибки {ex}");
+                    }
             }
+            
         }
 
         private void saveBT_Click(object sender, EventArgs e)
